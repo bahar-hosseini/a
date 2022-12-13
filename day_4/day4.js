ed0f64a8e71ee8b39e875aaf6e1eb26200d6323c
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-12-13 09:29:09 -0500

    silve part one of  date4

--- day_4/day4.js
+++ day_4/day4.js
@@ -4,6 +4,17 @@ const pathFile = path.join(__dirname, 'input.txt');
 
 fs.readFile(pathFile, (err, data) => {
   if (err) throw err;
+  let answer = 0;
   const array = data.toString().split('\n');
-  console.log(array);
+
+  for (let item of array) {
+    let i = item.split(/[-,]/);
+    if (
+      (Number(i[0]) <= Number(i[2]) && Number(i[1]) >= Number(i[3])) ||
+      (Number(i[2]) <= Number(i[0]) && Number(i[3]) >= Number(i[1]))
+    ) {
+      answer += 1;
+    }
+  }
+  console.log(answer);
 });
